f3bfaa440641f5f4ae327e512df5ffc3f6fdd176
jehova529 <55429509+jehova529@users.noreply.github.com>
2022-12-13 19:33:46 +0530

    initial commit

---
+++ src/test/java/com/net/pixel/PixelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.net.pixel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PixelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
