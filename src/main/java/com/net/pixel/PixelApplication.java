928215cf65cef4539eac5b432bfa22da4c878a1e
jehova.honey <jehova.honey@servicenow.com>
2022-12-14 11:18:49 +0530

    add test file

--- src/main/java/com/net/pixel/PixelApplication.java
+++ src/main/java/com/net/pixel/PixelApplication.java
@@ -1,9 +1,17 @@
 package com.net.pixel;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+
+import java.io.*;
+import java.util.Arrays;
+import java.util.stream.Collectors;
 
 @SpringBootApplication
+@Slf4j
 public class PixelApplication {
 
 	public static void main(String[] args) {
--- src/test/java/com/net/pixel/PixelApplicationTests.java
+++ src/test/java/com/net/pixel/PixelApplicationTests.java
@@ -1,13 +1,35 @@
 package com.net.pixel;
 
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.stream.Collectors;
 
 @SpringBootTest
+@Slf4j
 class PixelApplicationTests {
 
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	void print() throws IOException {
+		Resource resource = new ClassPathResource("test.txt");
+		File file = resource.getFile();
+		BufferedReader bufferedReader = new BufferedReader(new FileReader(file));
+
+		String[] array = bufferedReader.lines().collect(Collectors.joining()).split(",");
+		for(String s: array){
+			log.info(s);
+		}
 	}
 
 }
