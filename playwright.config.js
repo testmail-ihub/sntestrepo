334fb8e705ceb47bd3e43beb66069531faf51d2c
testmail-ihub <58020879+testmail-ihub@users.noreply.github.com>
2025-09-30 18:57:49 +0530

    Create Playwright configuration for Chromium, Firefox, and WebKit

---
+++ playwright.config.js
@@ -0,0 +1,9 @@
+const { defineConfig } = require('@playwright/test');
+
+module.exports = defineConfig({
+  projects: [
+    { name: 'Chromium', use: { browserName: 'chromium' } },
+    { name: 'Firefox', use: { browserName: 'firefox' } },
+    { name: 'WebKit', use: { browserName: 'webkit' } },
+  ],
+});
