d609bd699759f6696ed7ae768b8355df7a827f99
testmail-ihub <58020879+testmail-ihub@users.noreply.github.com>
2025-10-29 18:28:26 +0530

    Add WebDriverFactory.java for Selenium WebDriver management

---
+++ src/main/java/utils/WebDriverFactory.java
@@ -0,0 +1,35 @@
+package com.net.pixel.utils;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+import static com.net.pixel.config.ConfigManager.getBrowser;
+
+public class WebDriverFactory {
+
+    public static WebDriver getDriver() {
+        WebDriver driver;
+        String browser = getBrowser();
+
+        switch (browser.toLowerCase()) {
+            case "chrome":
+                WebDriverManager.chromedriver().setup();
+                driver = new ChromeDriver();
+                break;
+            case "firefox":
+                WebDriverManager.firefoxdriver().setup();
+                driver = new FirefoxDriver();
+                break;
+            case "edge":
+                WebDriverManager.edgedriver().setup();
+                driver = new EdgeDriver();
+                break;
+            default:
+                throw new IllegalArgumentException("Browser \"" + browser + "\" is not supported.");
+        }
+        return driver;
+    }
+}
