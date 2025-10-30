812dd0d3677c2c946a426d056fd343555a5a997a
testmail-ihub <58020879+testmail-ihub@users.noreply.github.com>
2025-10-30 16:00:12 +0530

    Add LoginPage.java for login flow automation

---
+++ src/main/java/com/example/automation/pages/LoginPage.java
@@ -0,0 +1,37 @@
+package com.example.automation.pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class LoginPage {
+    private WebDriver driver;
+
+    // Locators
+    private By usernameField = By.id("username");
+    private By passwordField = By.id("password");
+    private By loginButton = By.id("loginButton");
+
+    public LoginPage(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    // Actions
+    public void enterUsername(String username) {
+        driver.findElement(usernameField).sendKeys(username);
+    }
+
+    public void enterPassword(String password) {
+        driver.findElement(passwordField).sendKeys(password);
+    }
+
+    public void clickLoginButton() {
+        driver.findElement(loginButton).click();
+    }
+
+    public void login(String username, String password) {
+        enterUsername(username);
+        enterPassword(password);
+        clickLoginButton();
+    }
+}
